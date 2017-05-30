b6e808f62b3568fdadc43e4763619e5382097c00
Domink Sigmund <sigmund.dominik@googlemail.com>
2017-05-30 10:58:33 +0200

    Added Files and Folders to Config

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var cheerio = require('cheerio')
 var request = require('request')
 var moment = require('moment')
 var fs = require('fs')
+var path = require('path')
 var download = require('download')
 var gm = require('gm')
 var imageMagick = gm.subClass({imageMagick: true})
@@ -48,13 +49,13 @@ request(url, function (error, response, body) {
     // Download Images and mp3
     console.log('Downloading Image')
     download(data.image).then(data1 => {
-      fs.writeFileSync('tmp/image.jpg', data1)
+      fs.writeFileSync(path.join(config.paths.tmp, 'image.jpg'), data1)
       console.log('Downloading mp3')
       download(data.mp3).then(data2 => {
-        fs.writeFileSync('tmp/predigt.mp3', data2)
+        fs.writeFileSync(path.join(config.paths.tmp, 'predigt.mp3'), data2)
         // Create Front-Image using data and imagemagick
         console.log('Create Poster Image for movie')
-        imageMagick('tmp/image.jpg')
+        imageMagick(path.join(config.paths.tmp, 'image.jpg'))
           .resizeExact(720)
           .font('Helvetica.ttf', 36)
           .stroke('#000000', 1)
@@ -64,7 +65,7 @@ request(url, function (error, response, body) {
           .drawText(20, 80, data.bible)
           .drawText(20, 110, 'von ' + data.prediger)
           .drawText(590, 480, data.date.format('DD.MM.YYYY'))
-          .write('tmp/movie_image.jpg', function (err) {
+          .write(path.join(config.paths.tmp, 'movie_image.jpg'), function (err) {
             if (err) {
               console.error('error', err)
               process.exit(3)
@@ -74,9 +75,9 @@ request(url, function (error, response, body) {
                 'composite',
                 '-geometry 50x50+650+20',
                 '-quality', 100,
-                'assets/watermark.png',
-                'tmp/movie_image.jpg', // input
-                'tmp/movie_image_done.jpg'  // output
+                config.paths.watermark,
+                path.join(config.paths.tmp, 'movie_image.jpg'), // input
+                path.join(config.paths.tmp, 'movie_image_done.jpg')  // output
               ]
               execSync(command.join(' '), {stdio: [0, 1, 2]})
               // TODO: check result
@@ -85,20 +86,20 @@ request(url, function (error, response, body) {
 
               // convert predigt mp3 to aac file
               console.log('Converting predigt.mp3 to AAC')
-              execSync('ffmpeg -i tmp/predigt.mp3 -c:a aac -b:a 192k -strict -2 -y -v quiet -stats tmp/predigt.m4a', {stdio: [0, 1, 2]})
+              execSync('ffmpeg -i ' + path.join(config.paths.tmp, 'predigt.mp3') + ' -c:a aac -b:a 192k -strict -2 -y -v quiet -stats ' + path.join(config.paths.tmp, 'predigt.m4a'), {stdio: [0, 1, 2]})
               // TODO: check result
 
               // create movie using Front-Image and mp3
               console.log('Creating mp4-Clip from Image and Predigt')
-              execSync('ffmpeg -hide_banner -loglevel info -r 1 -loop 1 -i tmp/movie_image_done.jpg -i tmp/predigt.m4a -acodec copy -shortest -y -v quiet -stats tmp/movie.mp4', {stdio: [0, 1, 2]})
+              execSync('ffmpeg -hide_banner -loglevel info -r 1 -loop 1 -i ' + path.join(config.paths.tmp, 'movie_image_done.jpg') + ' -i ' + path.join(config.paths.tmp, 'predigt.m4a') + ' -acodec copy -shortest -y -v quiet -stats ' + path.join(config.paths.tmp, 'movie.mp4'), {stdio: [0, 1, 2]})
               // TODO: check result (is there a movie.mp4?)
 
               // Add VOrspann, Abspann
               console.log('Adding Vorspann and Abspann to mp4-Clip')
-              execSync('ffmpeg -i assets/vorspann.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts -y -v quiet -stats tmp/intermediate1.ts', {stdio: [0, 1, 2]})
-              execSync('ffmpeg -i tmp/movie.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts -y -v quiet -stats tmp/intermediate2.ts', {stdio: [0, 1, 2]})
-              execSync('ffmpeg -i assets/abspann.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts -y -v quiet -stats tmp/intermediate3.ts', {stdio: [0, 1, 2]})
-              execSync('ffmpeg -i "concat:tmp/intermediate1.ts|tmp/intermediate2.ts|tmp/intermediate3.ts" -c copy -bsf:a aac_adtstoasc -y -v quiet -stats output/' + data.date.format('YYYYMMDD') + '_movie.mp4', {stdio: [0, 1, 2]})
+              execSync('ffmpeg -i ' + config.paths.vorspann + ' -c copy -bsf:v h264_mp4toannexb -f mpegts -y -v quiet -stats ' + path.join(config.paths.tmp, 'intermediate1.ts'), {stdio: [0, 1, 2]})
+              execSync('ffmpeg -i ' + path.join(config.paths.tmp, 'movie.mp4') + ' -c copy -bsf:v h264_mp4toannexb -f mpegts -y -v quiet -stats ' + path.join(config.paths.tmp, 'intermediate2.ts'), {stdio: [0, 1, 2]})
+              execSync('ffmpeg -i ' + config.paths.abspann + ' -c copy -bsf:v h264_mp4toannexb -f mpegts -y -v quiet -stats ' + path.join(config.paths.tmp, 'intermediate3.ts'), {stdio: [0, 1, 2]})
+              execSync('ffmpeg -i "concat:' + path.join(config.paths.tmp, 'intermediate1.ts') + '|' + path.join(config.paths.tmp, 'intermediate2.ts') + '|' + path.join(config.paths.tmp, 'intermediate3.ts') + '" -c copy -bsf:a aac_adtstoasc -y -v quiet -stats ' + config.paths.output + data.date.format('YYYYMMDD') + '_movie.mp4', {stdio: [0, 1, 2]})
               // TODO: Check result
 
               // create youtube description (5000 max) as txt
@@ -117,14 +118,14 @@ request(url, function (error, response, body) {
               lines.push('Bibelstelle: ' + data.bible)
               lines.push('')
               lines.push(data.bible_text)
-              var description = fs.createWriteStream('output/' + data.date.format('YYYYMMDD') + '_description.txt')
+              var description = fs.createWriteStream(config.paths.output + data.date.format('YYYYMMDD') + '_description.txt')
               description.on('error', function (err) { console.error('Error', err) })
               lines.forEach(function (v) { description.write(v + '\n') })
               description.end()
 
               // create youtube tags as txt
               console.log('Creating YouTube-Tags')
-              var tags = fs.createWriteStream('output/' + data.date.format('YYYYMMDD') + '_tags.txt')
+              var tags = fs.createWriteStream(config.paths.output + data.date.format('YYYYMMDD') + '_tags.txt')
               tags.on('error', function (err) { console.error('Error', err) })
               tags.write(data.tags + ',' + config.youtube.additional_tags)
               tags.end()
@@ -133,15 +134,15 @@ request(url, function (error, response, body) {
 
               // cleanup tmp folder
               console.log('Cleaning Up TMP-Folder')
-              fs.unlinkSync('tmp/image.jpg')
-              fs.unlinkSync('tmp/intermediate1.ts')
-              fs.unlinkSync('tmp/intermediate2.ts')
-              fs.unlinkSync('tmp/intermediate3.ts')
-              fs.unlinkSync('tmp/movie_image_done.jpg')
-              fs.unlinkSync('tmp/movie_image.jpg')
-              fs.unlinkSync('tmp/movie.mp4')
-              fs.unlinkSync('tmp/predigt.m4a')
-              fs.unlinkSync('tmp/predigt.mp3')
+              fs.unlinkSync(path.join(config.paths.tmp, 'image.jpg'))
+              fs.unlinkSync(path.join(config.paths.tmp, 'intermediate1.ts'))
+              fs.unlinkSync(path.join(config.paths.tmp, 'intermediate2.ts'))
+              fs.unlinkSync(path.join(config.paths.tmp, 'intermediate3.ts'))
+              fs.unlinkSync(path.join(config.paths.tmp, 'movie_image_done.jpg'))
+              fs.unlinkSync(path.join(config.paths.tmp, 'movie_image.jpg'))
+              fs.unlinkSync(path.join(config.paths.tmp, 'movie.mp4'))
+              fs.unlinkSync(path.join(config.paths.tmp, 'predigt.m4a'))
+              fs.unlinkSync(path.join(config.paths.tmp, 'predigt.mp3'))
 
               console.log('Done. Go to the output-Folder and Upload the Video!')
             }
